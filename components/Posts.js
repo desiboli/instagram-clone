e1e276541eab9420617981485268bfdf6711e591
Mustafa Adam Alshammaa <adam.benals@gmail.com>
2021-12-25 18:17:50 +0100

    Posts

--- components/Posts.js
+++ components/Posts.js
@@ -1,5 +1,37 @@
+import Post from "./Post";
+
+const posts = [
+  {
+    id: "123",
+    username: "adamalshammaa",
+    userImg: "https://i.pravatar.cc",
+    img: "https://i.pravatar.cc",
+    caption: "Subscribe and destroy the like button for the me!",
+  },
+  {
+    id: "123",
+    username: "adamalshammaa",
+    userImg: "https://i.pravatar.cc",
+    img: "https://i.pravatar.cc",
+    caption: "Subscribe and destroy the like button for the me!",
+  },
+];
+
 function Posts() {
-  return <div></div>;
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Posts;

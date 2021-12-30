78283cfd813dcbb25863cdd918cc5f8ae5e0c7fd
Mustafa Adam Alshammaa <adam.benals@gmail.com>
2021-12-30 19:09:17 +0100

    Upload posts to firebase

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,12 @@ import {
 import { HomeIcon } from "@heroicons/react/solid";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modalAtom";
 
 function Header() {
   const { data: session } = useSession();
+  const [open, setOpen] = useRecoilState(modalState);
   const router = useRouter();
 
   return (
@@ -66,7 +69,10 @@ function Header() {
                 </div>
               </div>
 
-              <PlusCircleIcon className="navBtn" />
+              <PlusCircleIcon
+                onClick={() => setOpen(true)}
+                className="navBtn"
+              />
               <UserGroupIcon className="navBtn" />
               <HeartIcon className="navBtn" />
 
